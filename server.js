2d3bf72f2f19d7ade0918853f5185c0c29789ae7
Ismoil Vakhidov <braboev@gmail.com>
2026-02-27 10:44:39 +0500

    Update server listen address to 0.0.0.0 for external access

--- server.js
+++ server.js
@@ -147,6 +147,6 @@ app.post("/api/model", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
